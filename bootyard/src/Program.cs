b9d06bf10f6e6e9afccfdb83c7a94e9a2f8b5228
Anthony Walter <sysrpl@gmail.com>
2021-09-14 08:41:23 -0400

    Added test project

---
+++ bootyard/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bootyard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the test framework console");
+        }
+    }
+}
